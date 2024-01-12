832a2881dfd2a05449c32ef5589bc472f171570f
ElenaHudrea <elena.hudrea@yahoo.com>
2024-01-12 17:29:22 +0200

    WIP

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
+// navigation hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
 
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// FAQ
+const items = document.querySelectorAll(".question");
+
+function toggleAccordion() {
+  this.classList.toggle("active");
+  this.nextElementSibling.classList.toggle("active");
+}
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
