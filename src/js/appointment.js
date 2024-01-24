f10b9b4de57c954ef81472ecec029e619abec60b
ElenaHudrea <elena.hudrea@yahoo.com>
2024-01-24 13:44:09 +0200

    wip

--- src/js/appointment.js
+++ src/js/appointment.js
@@ -15,34 +15,45 @@ nextButton.addEventListener("click", () => {
       currentStep = steps.length;
     }
 
-    // Verificăm dacă există informații în localStorage
     if (localStorage.getItem("salonMembersInfo")) {
-      // Obținem informațiile din localStorage și le parsăm înapoi într-un array
       var salonMembers = JSON.parse(localStorage.getItem("salonMembersInfo"));
       var x = localStorage.getItem("ServiceSelected");
-      // Cream un nou array pentru a stoca membrii care oferă serviciul "Wash"
       var membersServiceSelected = [];
 
-      // Parcurgem fiecare membru din array
+      var container = document.querySelector(".stylist-cards");
       for (var i = 0; i < salonMembers.length; i++) {
-        // Verificăm dacă membrul oferă serviciul "Wash"
         if (salonMembers[i].services.includes(x)) {
-          // Dacă da, adăugăm membrul în noul array
           membersServiceSelected.push(salonMembers[i]);
+
+          (function (index) {
+            var divStylist = document.createElement("div");
+            divStylist.className = "stylist";
+            divStylist.id = "stylist-" + salonMembers[index].name.toLowerCase();
+            divStylist.onclick = function () {
+              selectStylist(
+                "stylist-" + salonMembers[index].name.toLowerCase()
+              );
+            };
+
+            var img = document.createElement("img");
+            img.src = salonMembers[index].imgName;
+            img.alt = salonMembers[index].name;
+            img.className = "img-stylist";
+            divStylist.appendChild(img);
+
+            var nameStylist = document.createElement("p");
+            nameStylist.textContent = salonMembers[index].name;
+            divStylist.appendChild(nameStylist);
+
+            container.appendChild(divStylist);
+          })(i);
         }
       }
-
-      // Afisăm în consolă membrii care oferă serviciul "Wash"
       console.log(membersServiceSelected);
-
-      // Dacă dorești să salvezi noul array în localStorage, poți utiliza:
-      // localStorage.setItem('membersWithWashService', JSON.stringify(membersWithWashService));
     } else {
-      console.log("Nu există informații în localStorage.");
+      console.log("Doesn't exist information in localStorage.");
     }
     updateProgress();
-  } else {
-    errorServiceMessage.style.display = "block";
   }
 });
 
@@ -80,7 +91,6 @@ function updateFormVisibility() {
 }
 
 var cardSelected = null;
-
 function selectService(id) {
   var cards = document.querySelectorAll(".card");
   cards.forEach(function (card) {
@@ -93,60 +103,23 @@ function selectService(id) {
   localStorage.setItem("ServiceSelected", cardSelected.innerText);
   return cardSelected;
 }
-
 function isServiceIsSelected() {
   return cardSelected !== null;
 }
 
-/////////////////////////////////////////////////
-// function afiseazaStilisti() {
-//   // Obțineți referința către containerul unde veți afișa informațiile despre stilisti
-//   var stilistiContainer = document.getElementById("stilistiContainer");
+var cardSelectedStylist = null;
+function selectStylist(id) {
+  var stylistCards = document.querySelectorAll(".stylist");
+  stylistCards.forEach(function (card) {
+    card.classList.remove("selected");
+  });
 
-//   // Obțineți serviciul selectat din local storage
-//   var serviceSelectat = localStorage.getItem("ServiceSelected");
+  cardSelectedStylist = document.getElementById(id);
+  cardSelectedStylist.classList.add("selected");
 
-//   // Obțineți variabila salonMembers din local storage
-//   var salonMembers = JSON.parse(localStorage.getItem("salonMembersInfo"));
-
-//   // Verificați dacă există informații despre stilisti
-//   if (salonMembers && Array.isArray(salonMembers)) {
-//     // Curățați conținutul anterior din containerul stilisti
-//     stilistiContainer.innerHTML = "";
-
-//     // Găsiți membrii care oferă serviciul selectat
-//     var membriCuServiciu = salonMembers.filter(function (membru) {
-//       return membru.services.includes(serviceSelectat);
-//     });
-
-//     // Afișați informațiile despre membrii care oferă serviciul selectat
-//     membriCuServiciu.forEach(function (membru) {
-//       var cardElement = document.createElement("div");
-//       cardElement.classList.add("card");
-
-//       var imagineElement = document.createElement("img");
-//       imagineElement.src = membru.imgName;
-//       imagineElement.alt = membru.name;
-//       imagineElement.classList.add("image-stylist");
-
-//       var numeElement = document.createElement("p");
-//       numeElement.textContent = membru.name;
-
-//       // Adăugați restul informațiilor (serviciu, preț, etc.) în funcție de nevoile dvs.
-
-//       // Adăugați cardElement la containerul stilisti
-//       stilistiContainer.appendChild(cardElement);
-//     });
-//   } else {
-//     console.log(
-//       "Nu există informații despre stilisti sau este un format incorect."
-//     );
-//     return;
-//   }
-
-//   // Aici continuați cu restul codului...
-//   // Curățați conținutul anterior, afișați stilisții, etc.
-// }
-
-// // Apelați funcția pentru a afișa stilistii
-// afiseazaStilisti();
+  // localStorage.setItem("StylistSelected", cardSelectedStylist.innerText);
+  return cardSelectedStylist;
+}
+function isStylistIsSelected() {
+  return cardSelectedStylist !== null;
+}
